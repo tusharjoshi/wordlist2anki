7d864c9444142f28e87c60041d125ee76fc7f286
Tushar Joshi <tusharvjoshi@gmail.com>
2019-01-12 10:32:20 +0530

    Refactor data-formatter.js to simplify code

--- src/data-formatter.js
+++ src/data-formatter.js
@@ -1,27 +1,56 @@
-module.exports = function (word, data, delimeter = '<br />') {
+function isDataAvailable(data) {
+    if( !data || !data.results) return false;
+    if( !data.results[0]) return false;
+    if( !data.results[0].lexicalEntries) return false;
+
+    return true;
+}
+
+function extractDefinition(lexicalEntry, delimeter) {
     var text = '';
-    if( data && data.results ) {
-        var result = data.results[0];
-        var lexicalEntries = result.lexicalEntries;
-  
-        for(let index = 0 ; index < result.lexicalEntries.length; index++) {
-            var item = result.lexicalEntries[index];
-            text += (index+1) + ': ' + item.lexicalCategory + delimeter;
-            for( var defindex = 0 ; defindex < item.entries[0].senses[0].definitions.length; defindex++) {
-                var def = item.entries[0].senses[0].definitions[defindex];
-                text += def + delimeter;
-            }
-            if( item.entries[0].senses[0].examples ) {
-                text += 'Examples:' + delimeter;
-                for( var exindex = 0 ; exindex < item.entries[0].senses[0].examples.length; exindex++) {
-                    var ex = item.entries[0].senses[0].examples[exindex];
-                    text += ex.text + delimeter;
-                }
-            }
-            text += delimeter;
-        }
+    var definitions = lexicalEntry.entries[0].senses[0].definitions;
+    definitions.forEach(function(def) {
+        text += def + delimeter;
+    });
+    return text;
+}
+
+function extractExamples(lexicalEntry, delimeter) {
+    var text = '';
+    var examples = lexicalEntry.entries[0].senses[0].examples;
+    if (examples) {
+        examples.forEach(function (example) {
+            text += example.text + delimeter;
+        });
+    }
+    return text;
+}
+
+function createTextForEntry(lexicalEntry, index, delimeter) {
+    var text = '';
+    try {
+        text += (index + 1) + ': ' + lexicalEntry.lexicalCategory + delimeter;
+        text += extractDefinition(lexicalEntry, delimeter);
+        text += extractExamples(lexicalEntry, delimeter);
+        text += delimeter;
+    } catch(ex) {
+        text += 'error in parsing.' + delimeter;
+    }
+    return text;
+}
+
+function createWordEntry(word, data, delimeter = '<br />') {
+    var text = '';
+    if( isDataAvailable(data) ) {
+        var lexicalEntries = data.results[0].lexicalEntries;
+        lexicalEntries.forEach(function(lexicalEntry, index){
+            text += createTextForEntry(lexicalEntry, index, delimeter);
+        });
     } else {
         text += 'no meaning found.' + delimeter;
     }
     return {"word": word, "text": text};
 }
+
+
+module.exports = createWordEntry; 
